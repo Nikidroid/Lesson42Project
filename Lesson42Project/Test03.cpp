943e80cde6ab5310152bd039255b4b2cdbbf1ae4
Krotovich Krot <for.games.straus3@gmail.com>
2024-02-20 17:55:05 +0300

    Add project files.

---
+++ Lesson42Project/Test03.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int size = 5;
+	int* array = NULL;
+
+	do {
+		cout << "Enter size of array: ";
+		cin >> size;
+	} while (size <= 0);
+
+	array = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		*(array + i) = rand() % 100;
+	}
+
+	for (int i = 0; i < size; i++) {
+		cout << (array + i) << " - " << *(array + i) << endl;
+	}
+
+	delete[] array;
+
+	return 0;
+}
